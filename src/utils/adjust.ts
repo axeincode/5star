a9da635d3118babbfac3f96a1062cb39a4280445
Full Stack Developer <114348312+sniper19891112@users.noreply.github.com>
2024-03-18 18:42:36 +0100

    updated_0318

--- src/utils/adjust.ts
+++ src/utils/adjust.ts
@@ -1,6 +1,16 @@
 
 import Adjust from "@adjustcom/adjust-web-sdk";
 
-export function adjustTrackEvent(param: any): void {
-    Adjust.trackEvent(param);
+(window as any)["AndroidWebView"].firebaseEvent = function(key: any, value: any) {}
+
+function isRunningAsMobileWebApp(): boolean {
+  return window.matchMedia('(display-mode: standalone)').matches || (navigator as any).standalone;
+}
+
+export function adjustTrackEvent(key: any, value = null as any): void {
+  if (isRunningAsMobileWebApp()) {
+    (window as any)["AndroidWebView"].firebaseEvent(key, value)
+  } else  {
+    Adjust.trackEvent(key);
+  }
 }
