c6b54f450f5ff4a38a7ce9cdddcf9fd77f91425b
Full Stack Developer <legnonoda@gmail.com>
2024-03-21 10:05:17 +0100

    updated

--- src/utils/adjust.ts
+++ src/utils/adjust.ts
@@ -5,10 +5,36 @@ function isRunningAsMobileWebApp(): boolean {
   return window.matchMedia('(display-mode: standalone)').matches || (navigator as any).standalone;
 }
 
+/**
+ * Determine the mobile operating system.
+ * This function returns one of 'iOS', 'Android', 'Windows Phone'.
+ *
+ * @returns {boolean}
+ */
+function getMobileOperatingSystem(): boolean {
+  var userAgent = navigator.userAgent;
+
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+    return true;
+  }
+
+  if (/android/i.test(userAgent)) {
+    return true;
+  }
+
+  // iOS detection
+  if (/iPad|iPhone|iPod/.test(userAgent)) {
+    return true;
+  }
+
+  return false;
+}
+
 export function adjustTrackEvent(key: any, value = null as any): void {
-  if (isRunningAsMobileWebApp()) {
+  if (getMobileOperatingSystem()) {
     (window as any)["AndroidWebView"].firebaseEvent(key, value)
-  } else  {
+  } else {
     Adjust.trackEvent(key);
   }
 }
